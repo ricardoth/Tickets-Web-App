46dbc6a57d5e17087667feac91f5ebeaef3ba520
ricardoth <ricardotilleriaochoa@gmail.com>
2023-09-14 17:38:49 -0300

    Nueva pantalla para leer desde un dispositivo externo los tickets generados en la web

---
+++ src/components/lecturaTickets/LecturaTickets.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const LecturaTickets = () => {
+    return (
+        <div className='row mt-5'>
+                <div className='d-flex justify-content-between'>
+                    <h1>Identificación de Tickets</h1>
+                </div>
+                <hr/>
+        </div>
+    )
+}
